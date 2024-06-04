ff03c8fee0b02f61c3aafbc3153ff2c4f0ee31aa
Hindi <Tzachi.Hindi@cognyte.com>
2024-06-04 12:20:05 +0300

    exe2_memory

--- MemoryGame.py
+++ MemoryGame.py
@@ -1,6 +1,4 @@
 import random
-diff=5
-
 def generate_sequence(diff):
     list_of_number = []
     i = 0
@@ -13,7 +11,7 @@ def get_guess_from_user(diff):
     guess_number = []
     i = 0
     while i < diff:
-        guess_number.append(int(input(f"What is your guess number from 1 to {diff}:")))
+        guess_number.append(int(input(f"What is your memorize number from 1 to 101:")))
         i = i + 1
     return guess_number
 
@@ -24,17 +22,18 @@ def compare_results(list_of_number, guess_number):
 
 
 def play(diff):
-    x = generate_number(diff)
+    x = generate_sequence(diff)
     print(x)
     y = get_guess_from_user(diff)
     print(y)
     compare_results(x, y)
 
 
-x = (generate_sequence(diff))
-print(x)
-y = (get_guess_from_user(diff))
-print(x)
-print(y)
-
-compare_results(x, y)
+# x = generate_sequence(diff)
+# print(x)
+# y = get_guess_from_user(diff)
+# print(x)
+# print(y)
+#
+# compare_results(x, y)
+# play(5)
